70d382e1c4e6c7dbe8c61a3bc05477e1736acc4e
praveen dodla <dodlapraveen09@gmail.com>
2022-07-30 18:35:18 +0530

    basic core programs

---
+++ FlipCoin/FlipCoin/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+<<<<<<< HEAD
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's flip a coin 10000 times");
+=======
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's flip a coin 100 times");
+>>>>>>> UC1_FlipCoin
+            Coinflip();
+        }
+
+        private static int Coinflip()
+        {
+            Random Rand = new Random();
+            int heads = 0;
+            int tails = 0;
+            int lastResult = 0;
+            int numOfTriples = 0;
+            int currenStreak = 0;
+            int result = 0;
+
+
+<<<<<<< HEAD
+            for (int i = 0; i < 10000; i++)
+=======
+            for (int i = 0; i < 100; i++)
+>>>>>>> UC1_FlipCoin
+            {
+                result = Rand.Next(0, 2);
+
+                if (result == 1)
+                {
+                    lastResult = result;
+                    heads++;
+                }
+                else
+                {
+                    lastResult = 0;
+                    tails++;
+                }
+
+                if (lastResult == result)
+                {
+                    currenStreak++;
+
+                    if (currenStreak == 3)
+                    {
+                        numOfTriples++;
+                        currenStreak = 0;
+                    }
+                    else
+                    {
+                        currenStreak = 0;
+                    }
+                }
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+            Console.WriteLine("tails was flipped {0} times", tails);
+            return result;
+        }
+    }
+}
---
+++ PowerOf2/PowerOf2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 8;
+
+            Program p = new Program();
+            bool flag = false;
+            flag = p.isPowerOf2(num);
+
+            if (flag == true)
+            {
+                Console.WriteLine("{0} is a number that is the power of 2.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not the power of 2.", num);
+            }
+        }
+        Boolean isPowerOf2(int number)
+        {
+            while (number != 1)
+            {
+                if (number % 2 != 0)
+                    return false;
+                number = number / 2;
+            }
+            return true;
+        }
+    }
+}
